26a7bb4a08d49b2e931998fec4cb8243b9301f8b
Pynckels <filip.pynckels@telenet.Be>
2025-02-10 15:36:45 +0100

    Fix for issue #1 & Check fontfile type

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -2,9 +2,10 @@
 
 set -x
 
-../prt1403 -j TEST1   -y -u PRT1403 -s WIDE   -c GREEN -o testOut/test1.pdf testIn/test1.txt
-../prt1403 -j PI      -y -u FRTRN77 -s SMALL  -c BLUE  -o testOut/test2.pdf testIn/test2.f77
-../prt1403 -j TEST3   -y -u PRT1403 -s MEDIUM -c GRAY  -o testOut/test3.pdf testIn/test3.txt
-../prt1403 -j BNCHMRK -y -u FRTRN90 -s SMALL  -c BLUE  -o testOut/test4_1.pdf testIn/test4.f90
-../prt1403 -j BNCHMRK -y -u FRTRN90 -s SMALL  -c WHITE  -f FONT1403 -o testOut/test4_2.pdf testIn/test4.f90
-../prt1403 -y -f '/usr/share/fonts/truetype/freefont/FreeMono.ttf' -p 12 -o testOut/test4_3.pdf testIn/test4.f90
+../prt1403 -y -j TEST1   -u PRT1403 -s WIDE   -c GREEN -o testOut/test1.pdf testIn/test1.txt
+../prt1403 -y -j PI      -u FRTRN77 -s SMALL  -c BLUE  -o testOut/test2.pdf testIn/test2.f77
+../prt1403 -y -j TEST3   -u PRT1403 -s MEDIUM -c GRAY  -o testOut/test3.pdf testIn/test3.txt
+../prt1403 -y -j BNCHMRK -u FRTRN90 -s SMALL  -c BLUE  -o testOut/test4_1.pdf testIn/test4.f90
+../prt1403 -y -j BNCHMRK -u FRTRN90 -s SMALL  -c WHITE  -f FONT1403 -o testOut/test4_2.pdf testIn/test4.f90
+../prt1403 -y -j BNCHMRK -u FRTRN90 -f '/usr/share/fonts/truetype/freefont/FreeMono.ttf' -p 12 -o testOut/test4_3.pdf testIn/test4.f90
+../prt1403 -y -n -s SMALL  -c BLUE  -o testOut/test4_4.pdf testIn/test4.f90
