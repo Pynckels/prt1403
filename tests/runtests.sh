73e65488dcf8ac87bca1598502bf0125c550fefb
Pynckels <filip.pynckels@telenet.Be>
2025-02-13 17:52:00 +0100

    First draft for separator page

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -10,3 +10,6 @@ set -x
 ../prt1403 -y    -j BNCHMRK -u FRTRN90 -s WIDE   -c GREEN -f '/usr/share/fonts/truetype/freefont/FreeMono.ttf' -p 12 -o testOut/test4_3.pdf testIn/test4.f90
 ../prt1403 -y    -j BNCHMRK -u FRTRN90 -s SMALL  -c GREEN -f '/usr/share/fonts/truetype/Jellyka_Estrya_Handwriting/Jellyka_Estrya_Handwriting.ttf' -p 38 -o testOut/test4_4.pdf  testIn/test4.f90
 ../prt1403 -y -n                       -s SMALL  -c BLUE  -o testOut/test4_5.pdf testIn/test4.f90
+../prt1403 -y -e -j BNCHMRK -u FRTRN90 -s SMALL  -f FONT1403 -o testOut/test4_6.pdf testIn/test4.f90
+../prt1403 -y -e -j BNCHMRK -u FRTRN90 -s MEDIUM -o testOut/test4_7.pdf testIn/test4.f90
+../prt1403 -y -e -j BNCHMRK -u FRTRN90 -s WIDE   -o testOut/test4_8.pdf testIn/test4.f90
